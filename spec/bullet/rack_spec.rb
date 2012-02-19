f8d2bfe359a3d90699a9ea1600ac21b7acefd33b
Duncan Beevers <duncan@dweebd.com>
2012-02-19 06:01:17 -0600

    Basic specs for Bullet::Rack

---
+++ spec/bullet/rack_spec.rb
@@ -0,0 +1,33 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+
+describe Bullet::Rack do
+  let(:middleware) { Bullet::Rack.new app }
+  let(:app) { AppDouble.new }
+
+  describe "#call" do
+    context "when Bullet is enabled" do
+      before(:each) { Bullet.enable = true }
+
+      it "should invoke Bullet.start_request" do
+        Bullet.should_receive(:start_request)
+        middleware.call([])
+      end
+
+      it "should invoke Bullet.end_request" do
+        Bullet.should_receive(:end_request)
+        middleware.call([])
+      end
+    end
+
+    context "when Bullet is disabled" do
+      before(:each) { Bullet.enable = false }
+      after(:each) { Bullet.enable = true }
+
+      it "should not call Bullet.start_request" do
+        Bullet.should_not_receive(:start_request)
+        middleware.call([])
+      end
+    end
+  end
+end
